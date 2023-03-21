b5efc7ea59999059a63b6f1c3d63c846f78ec69f
btork2022 <bentork2010@gmail.com>
2023-03-21 21:38:43 +0000

    tweaked codes and comments on Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,46 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 
-const Header = () => {
-    return ( 
-    <header className="header">
-    <Link to="/">Home</Link>
-    <Link to="contact">Contact</Link>
-    <Link to="project">Project</Link>
-    </header>
-    );
-};
+// const Header = () => {
+//     return ( 
+//     <header className="header">
+//     <Link to="/">Home</Link>
+//     <Link to="contact">Contact</Link>
+//     <Link to="project">Project</Link>
+//     </header>
+//     );
+// };
+
+// export default Header;
+
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Header.css';
+
+function Header() {
+  return (
+    <nav>
+      <div className="container">
+        {/* Logo and name */}
+        <div className="logo">
+          <NavLink to="/">My Portfolio</NavLink>
+        </div>
+        {/* Navigation links */}
+        <ul className="nav-links">
+          <li>
+            <NavLink to="/project" activeClassName="active">
+              Projects
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" activeClassName="active">
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
 
 export default Header;
