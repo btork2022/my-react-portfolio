62900ac0d503f6f4b453ff0158286b804605af63
btork2022 <bentork2010@gmail.com>
2023-03-21 21:40:28 +0000

    tweaked codes and comments on Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,49 @@
 
-const Home = ({name, title}) => {
+// const Home = ({name, title}) => {
     
-    return <div>
+//     return <div>
         
-        <section className="splash">
-            <section>
-            <img src="https://images.unsplash.com/photo-1608306448197-e83633f1261c?ixlib
-            =rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fGRldmVsb3BlcnxlbnwwfHwwfHw%3D&auto
-            =format&fit=crop&w=500&q=60"/>
-            </section>
-            <section>
-                <h1>My name is {name}</h1>
-                <p>Title: {title}  </p>
-            </section>
-        </section>
+//         <section className="splash">
+//             <section>
+//             <img alt="" src="../assets/img/IMG-1977.JPG"/>
+//             </section>
+//             <section>
+//                 <h1>My name is {name}</h1>
+//                 <p>Title: {title}  </p>
+//             </section>
+//         </section>
+//     </div>
+// }
+
+// export default Home;
+
+
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home-container">
+      {/* Name and headshot */}
+      <div className="name-container">
+        <h1>John Doe</h1>
+        <img
+          src="../assets/img/IMG-1977.JPG"
+          alt="John Doe"
+          className="headshot"
+        />
+      </div>
+      {/* Brand statement and indication of portfolio site */}
+      <div className="brand-container">
+        <p className="brand-statement">
+          I am a Front-end web developer with experience in React, Node.js, Javascript, CSS & HTML.
+        </p>
+        <p className="portfolio-indicator">
+          Welcome to my portfolio website!
+        </p>
+      </div>
     </div>
+  );
 }
 
 export default Home;
