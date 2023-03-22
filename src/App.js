67031029aade43365439642ca01cc2da8eb87fa9
btork2022 <bentork2010@gmail.com>
2023-03-22 21:12:32 +0000

    modified page background height

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Gallery from './components/Gallery';
-import Header from './components/Header';
+import Header from './components/Header'
 
 function App() {
   return (
