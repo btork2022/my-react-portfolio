1533dde3e3007e29c09c537044b00bd86802c7aa
btork2022 <bentork2010@gmail.com>
2023-03-22 17:17:44 +0000

    updated code in index.*, Home.js & Project.css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,50 @@
 
+
+import React from 'react';
+import '../styles/Home.css'
+
+function Home() {
+  return (
+    <div className="home-container">
+      {/* Name and headshot */}
+      <div className="name-container">
+        <h1>Ben Torkornoo</h1>
+        <img
+          src="../assets/img/IMG-1977.JPG"
+          alt="John Doe"
+          className="headshot"
+        />
+      </div>
+      {/* My Brand statement*/}
+      <div className="brand-container">
+        <p className="brand-statement">
+          I am a Front-end web developer with experience in React, Node.js, Javascript, CSS & HTML.
+        </p>
+        <p className="portfolio-indicator">
+          Welcome to my portfolio website!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// OLD CODE
+
 // const Home = ({name, title}) => {
     
 //     return <div>
@@ -16,34 +62,3 @@
 // }
 
 // export default Home;
-
-
-import React from 'react';
-import '../styles/Home.css'
-
-function Home() {
-  return (
-    <div className="home-container">
-      {/* Name and headshot */}
-      <div className="name-container">
-        <h1>John Doe</h1>
-        <img
-          src="../assets/img/IMG-1977.JPG"
-          alt="John Doe"
-          className="headshot"
-        />
-      </div>
-      {/* Brand statement and indication of portfolio site */}
-      <div className="brand-container">
-        <p className="brand-statement">
-          I am a Front-end web developer with experience in React, Node.js, Javascript, CSS & HTML.
-        </p>
-        <p className="portfolio-indicator">
-          Welcome to my portfolio website!
-        </p>
-      </div>
-    </div>
-  );
-}
-
-export default Home;
